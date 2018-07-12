f76186f21cceb38ea968323d98d15b8f8eee51b8
John Bonaguro <jbonaguro2@gmail.com>
2018-07-11 21:56:28 -0400

    Added officers and service pages

--- LCA_Website_Node/routes/index.js
+++ LCA_Website_Node/routes/index.js
@@ -1,19 +1,36 @@
 var express = require('express');
 var router = express.Router();
+var fs = require('fs');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index');
 });
 
-
 router.get('/officers', function(req, res, next) {
-  res.render('officers');
-});
+  let exec = fs.readFileSync("./views/exec.csv");
+  exec = exec.toString().split('\n').map((line) => {
+    return line.split(',');
+  });
+  console.log(exec);
 
+  let officers = fs.readFileSync("./views/officers.csv");
+  officers = officers.toString().split('\n').map((line) => {
+      return line.split(',');
+  });
+  console.log(officers);
+  res.render('officers', {
+    exec: exec,
+    officers: officers
+  });
+});
 
 router.get('/scholarship', function(req, res, next) {
   res.render('scholarship');
+});
+
+router.get('/service', function(req, res, next) {
+    res.render('service');
 });
 
 module.exports = router;
